ec1ed41a0f4844930630f02da1d23a908099b48e
Yehor Akunishnikov <faiteforever@gmail.com>
2022-07-09 17:14:05 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const {push} = require("karma/lib/init/log-queue");
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:4200",
